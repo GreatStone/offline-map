07eacbaa379b222063c88f83a009d953a2e06d47
GreatStone <12281054@bjtu.edu.cn>
2015-08-15 13:21:28 +0800

    my bitset class done, support common set operation

---
+++ bit_num.cpp
@@ -0,0 +1,102 @@
+#include "bit_num.h"
+
+#include <stdio.h>
+#include <string.h>
+
+namespace utils {
+
+BitNum::BitNum(const BitNum & src) {
+    _bit_sz = src._bit_sz;
+    int sz = src.get_grp_sz();
+    _data = new unit[sz];
+    memcpy(_data, src._data, sizeof(unit) * sz);
+}
+
+void BitNum::reset(int index) {
+    _data[index / 32] &= _zeros[index % 32];
+}
+
+void BitNum::reset() {
+    for (int i = 0; i < get_grp_sz(); ++i) {
+	_data[i] = _all_none;
+    }
+}
+
+void BitNum::set(int index) {
+    _data[index / 32] |= _ones[index % 32];
+}
+
+void BitNum::set() {
+    for (int i = 0; i < get_grp_sz(); ++i) {
+	_data[i] = _all_set;
+    }
+}
+
+void BitNum::flip(int index) {
+    _data[index / 32] ^= _ones[index % 32];
+}
+
+void BitNum::flip() {
+    for (int i = 0; i < get_grp_sz(); ++i) {
+	_data[i] = ~_data[i];
+    }
+}
+
+inline bool BitNum::test(int index) {
+    return _data[index / 32] & _ones[index % 32];
+}
+
+BitNum BitNum::operator & (const BitNum & x) const {
+    BitNum ret(_bit_sz);
+    int sz = get_grp_sz();
+    for (int i = 0; i < sz; ++i) {
+	ret._data[i] = _data[i] & x._data[i];
+    }
+    return ret;
+}
+
+BitNum BitNum::operator | (const BitNum & x) const {
+    BitNum ret(_bit_sz);
+    int sz = get_grp_sz();
+    for (int i = 0; i < sz; ++i) {
+	ret._data[i] = _data[i] | x._data[i];
+    }
+    return ret;
+}
+
+BitNum BitNum::operator - (const BitNum & x) const {
+    BitNum ret(_bit_sz);
+    int sz = get_grp_sz();
+    for (int i = 0; i < sz; ++i) {
+	ret._data[i] = (_data[i] ^ x._data[i]) & _data[i];
+    }
+    return ret;
+}
+
+BitNum BitNum::operator ~ () const {
+    BitNum ret(*this);
+    int sz = get_grp_sz();
+    for (int i = 0; i < sz; ++i) {
+	ret._data[i] = ~ret._data[i];
+    }
+    return ret;
+}
+
+bool BitNum::operator < (const BitNum & x) const {
+    int sz = get_grp_sz();
+    for (int i = sz - 1; i >= 0; i++) {
+	if (_data[i] < x._data[i]) {
+	    return true;
+	} else if (_data[i] > x._data[i]) {
+	    return false;
+	}
+    }
+    return false;
+}
+
+}
+
+
+int main () {
+    return 0;
+}
